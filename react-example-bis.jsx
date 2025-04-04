71bb2dad60263e20fc33facd204b5b07c2676ed8
FW <forrest323@icloud.com>
2025-04-03 20:12:31 -0600

    added comments to code

--- react-example-bis.jsx
+++ react-example-bis.jsx
@@ -1,21 +1,32 @@
+// Import react hooks necessary to make CRUD actions functional
 import { useState, useEffect, useRef } from 'react';
+
+// Import axios library to communicate with database API
 import axios from 'axios';
 
+// Assign API URL to a variable for ease of handling
 const url = 'http://localhost:3000/posts'
 
+// Declare react component 
 const PostDataExample = () => {
-////////////////////
+  // Declare specific useState hooks necessary to keep track of state between renders
   const [post, setPost] = useState({title: "", body: ""})
   const [posts, setPosts] = useState([])
   const [editState, setEditState] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
   const [isError, setIsError] = useState(false)
+
+  // useRef hook is used to maintain focus on text input field 
   const inputRef = useRef(null)
 
+  // This function is responsible for appending new posts and edited posts to the client's local copy of the post list in addition to the server's database copy
   const createPost = async (event) => {
     event.preventDefault()
     if (post.title !== "" && post.body !== "") {
+
+      // If the current post is a new post the application we follow the first branch of the if statement
       if (editState === null) {
+        // Axios is used to append the new post to the database. If the append is successful we use setPosts to append the new post to the client's local copy of the post list.
         try {
           const { data } = await axios.post(url, post, {headers: { "Content-Type": "application/json" }})
           console.log("Created post: ", data)
@@ -29,7 +40,10 @@ const PostDataExample = () => {
         } catch (error) {
           console.log("Error creating post: ", error)
         } 
+
+        // If the current post is an existing one to be edited we follow the second branch of the if statement 
         } else {
+          // A try-catch block is used to edit the database's copy of the post list first. If the edit is successful we update the local copy via setPosts.
           try {
             const { data } = await axios.put(url+"/"+post.id, post, {headers: {"Content-Type": "application/json"}})
             console.log("Edited post: ", data)
@@ -52,7 +66,7 @@ const PostDataExample = () => {
       }
     }
 
-
+  // This function displays all of the posts currently present within the posts array instantiated by useState by mapping over them
   const RenderPosts = () => {
     return (
       posts.map((element, index) => {
@@ -68,12 +82,14 @@ const PostDataExample = () => {
     )
   }
 
+  // This function activates when the user clicks the 'edit' button. It sets the current post to be edited and the editState variable to the post's id. 
   const updatePost = ({title, body, id}) => {
     setPost({title, body, id})
     setEditState(id)
     textFocus()
   }
 
+  // This function deletes a given post by first deleting the database copy via the axios delete method. It then proceeds to delete the local copy of the post by filtering through them and looking for a matching id. 
   const deletePost = async (id) => {
     try {
       const { data } = await axios.delete(url+"/"+id, {headers: {"Content-Type": "application/json"}})
@@ -93,6 +109,7 @@ const PostDataExample = () => {
     }
   }
 
+  // This function retreives all of the posts from the database using an axios get request. A try/catch block is used for error handling.
   const fetchPosts = async () => {
     try {
       const { data } = await axios.get(url, {headers: { "Accept": "application/json" }})
@@ -108,12 +125,14 @@ const PostDataExample = () => {
     }
   }
 
+  // This function is only available to the user when edit mode is activated. The function turns edit mode off and sets the current post to blank. 
   const cancelEdit = () => {
     setEditState(null)
     setPost({title: "", body: ""})
     textFocus()
   }
 
+  // This function ensures data consistency between the current useState of the post and what the user has entered into the page's form
   const handleChange = (event) => {
     const { name, value } = event.target
 
@@ -125,20 +144,24 @@ const PostDataExample = () => {
     })
   }
 
+  // This function changes the browser's focus to the first input text field
   const textFocus = () => {
     inputRef.current?.focus()
   }
 
+  // This useEffect ensures that posts are retrieved from the database after initial render 
   useEffect(() => {
     fetchPosts()
   },[])
 
+  // This useEffect ensures that the focus of the application defaults to the first input text field upon initial render and when the loading and error variables change
   useEffect(() => {
     if (!isLoading && !isError) {
       textFocus()
     }
   },[isLoading, isError])
 
+  // This is the first of three conditional renders. This h1 will render if the application is still loading posts from the database.
   if (isLoading) {
     return (
       <>
@@ -147,6 +170,7 @@ const PostDataExample = () => {
     )
   }
 
+  // This is the second of three conditional renders. This h1 will render if the application has encountered and error. 
   if (isError) {
     return (
       <>
@@ -155,10 +179,15 @@ const PostDataExample = () => {
     )
   }
 
+  // This is the final conditional render. This block of JSX will render when there are no errors and the posts have finished loading
   return (
     <>
       <h2>Welcome to the post machine!</h2>
+
+      {/* This component renders all of the posts from the database by mapping over them arranged in an array */}
       <RenderPosts />
+
+      {/* This form is where users can create new posts and edit existing ones. */}
       <form onSubmit={createPost}>
         <input ref={inputRef} type="text" name="title" value={post.title} onChange={handleChange} />
         <input type="text" name="body" value={post.body} onChange={handleChange} />
@@ -167,8 +196,8 @@ const PostDataExample = () => {
       {editState !== null && <button onClick={cancelEdit}>Cancel Edit</button>}
     </>
   )
-////////////////////
 };
 
+// This export statement allows the PostDataExample component to be imported by other components
 export default PostDataExample;
 
